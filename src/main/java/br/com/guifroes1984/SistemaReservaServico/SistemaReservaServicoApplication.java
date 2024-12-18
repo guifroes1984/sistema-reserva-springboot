23afc6a8820add10b67a098f7742acc293478879
guifroes1984 <guilhermefroes26@gmail.com>
2024-12-17 23:59:38 -0300

    Criação da API e configuração do BD

---
+++ src/main/java/br/com/guifroes1984/SistemaReservaServico/SistemaReservaServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guifroes1984.SistemaReservaServico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservaServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservaServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guifroes1984/SistemaReservaServico/SistemaReservaServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guifroes1984.SistemaReservaServico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservaServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
